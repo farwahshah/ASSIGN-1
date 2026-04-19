1ec37929238ea5535fd8e003fbd75cf52fe5bc0c
farwahshah <farwa2138@gmail.com>
2026-04-19 16:01:48 +0500

    Add files via upload

--- smsproject.cpp
+++ smsproject.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 main(){
-string st1_name ,st2_name;
-float st1_fsc , st1_sms1 ,st2_sms1 ,st2_fsc ,;
-
-cout<<"******************************************************************"<<endl;
+string st1_name,st2_name;
+float st1_fsc, st1_sms1, st2_sms1 ,st2_fsc, st3_fsc, st3_sms1, st4_sms1, st4_fsc, st5_sms1, st5_fsc, st6_sms1, st6_fsc,;
+float total_marks =1100; // or whatever value
+float total_GPA =4.0
+cout<<"******************************************************************"<<endl; 
 cout<<"*          scholarships Management system                        *"<<endl;
 cout<<"******************************************************************"<<endl;
 
@@ -412,12 +413,12 @@ if(option==7){
 cout<<"you cannot add more information"<<endl;
 }
 if (option ==8){
-cout<<"st1_name<<"\t"<<st1_fsc<<"\t"<<st1_semester_GPA<<endl;
-cout<<"st2_name<<"\t"<<st2_fsc<<"\t"<<st1_semester_GPA<<endl;
-cout<<"st3_name<<"\t"<<st3_fsc<<"\t"<<st1_semester_GPA<<endl;
-cout<<"st4_name<<"\t"<<st4_fsc<<"\t"<<st1_semester_GPA<<endl;
-cout<<"st5_name<<"\t"<<st5_fsc<<"\t"<<st1_semester_GPA<<endl;
-cout<<"st6_name<<"\t"<<st6_fsc<<"\t"<<st1_semester_GPA<<endl;
+cout<<"st1_name"<<"\t"<<st1_fsc<<"\t"<<"st1_semester_GPA"<<endl;
+cout<<"st2_name"<<"\t"<<st2_fsc<<"\t"<<"st1_semester_GPA"<<endl;
+cout<<"st3_name"<<"\t"<<st3_fsc<<"\t"<<"st1_semester_GPA"<<endl;
+cout<<"st4_name"<<"\t"<<st4_fsc<<"\t"<<"st1_semester_GPA"<<endl;
+cout<<"st5_name"<<"\t"<<st5_fsc<<"\t"<<"st1_semester_GPA"<<endl;
+cout<<"st6_name"<<"\t"<<st6_fsc<<"\t"<<"st1_semester_GPA"<<endl;
 }
 if( option ==9){
 cout<<"you have no record yet"<<endl;
@@ -471,12 +472,12 @@ cout<<"you cannot add more information"<<endl;
 }
 
 if(option==9){
-float merit1 =st1_fsc / total_marks*40 +st1_semester_GPA /total_GPA*60;
-float merit2 =st2_fsc / total_marks*40 +st2_semester_GPA /total_GPA*60;
-float merit3 =st3_fsc / total_marks*40 +st3_semester_GPA /total_GPA*60;
-float merit4 =st4_fsc / total_marks*40 +st4_semester_GPA /total_GPA*60;
-float merit5 =st5_fsc / total_marks*40 +st5_semester_GPA /total_GPA*60;
-float merit6 =st6_fsc / total_marks*40 +st6_semester_GPA /total_GPA*60;
+float merit1 = (st1_fsc / total_marks)*40 + (st1_semester_GPA /total_GPA)*60;
+float merit2 = (st2_fsc / total_marks)*40 + (st2_semester_GPA /total_GPA)*60;
+float merit3 = (st3_fsc / total_marks)*40 + (st3_semester_GPA /total_GPA)*60;
+float merit4 = (st4_fsc / total_marks)*40 + (st4_semester_GPA /total_GPA)*60;
+float merit5 = (st5_fsc / total_marks)*40 + (st5_semester_GPA /total_GPA)*60;
+float merit6 = (st6_fsc / total_marks)*40 + (st6_semester_GPA /total_GPA)*60;
 
   if(merit1>merit2){
     cout<<"your application for scholarship is submit "<<endl;
@@ -549,7 +550,11 @@ cout<<"you are eligible for a scholarshipe"<<endl;
 if (option==11){
 cout<<exit<<endl;
 }
-
+}
+ }
+}
+   }
+  }}}
 
 
 
