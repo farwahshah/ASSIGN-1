bc104f329ba6b5486375b388ec6bcd3fc950415f
farwahshah <farwa2138@gmail.com>
2026-04-19 14:54:44 +0500

    Add files via upload

---
+++ smsproject.cpp
@@ -0,0 +1,556 @@
+#include <iostream>
+using namespace std;
+main(){
+string st1_name ,st2_name;
+float st1_fsc , st1_sms1 ,st2_sms1 ,st2_fsc ,;
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl;
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+int option;
+cin>>option;
+ 
+if( option ==1){
+ cout <<"Enter the student name: "<<endl;
+ cout<<"Enter the FSC marks: "<<endl;
+ cout<<"Enter the total marks: "<<endl;
+ cout<<"Enter the semester_1 GPA: "<<endl;
+ cout<<"Enter the total GPA: "<<endl;
+
+if(option ==2){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==3){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==4){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==5){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==6){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==7){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==8){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==9){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+}
+
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl; 
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+cin>>option;
+
+if(option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==2){
+cout<<"Enter Your Father name: "<<endl;
+cout<<"Enter Your CNIC : "<<endl;
+cout<<"Enter Domicile District: "<<endl;
+cout<<"Enter the name of town/village/chak: "<<endl;
+}
+if( option ==3){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==4){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==5){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==6){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==7){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==8){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==9){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+}
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl;
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+cin>>option;
+
+if( option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if( option ==2){
+cout<<"you cannot add more information"<<endl;
+}
+if( option ==3){
+cout<<"Enter the University Name: "<<endl;
+cout<<"Enter the Department Name/Degree Title: "<<endl;
+cout<<"Enter the semester year: "<<endl;
+cout<<"Enter University Registeration No: "<<endl;
+}
+if(option ==4){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==5){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==6){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==7){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==8){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==9){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+}
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl; 
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+cin>>option;
+
+if(option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==2){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==3){ 
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==4)
+ cout<<"Enter the Hobby/Activity name: "<<endl;
+}
+if( option ==5){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==6){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==7){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==8){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==9){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl;
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+cin>>option;
+
+if(option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==2){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==3){ 
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==4)
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==5){
+cout<<"Enter the family income(Montly): "<<endl;
+cout<<"Enter other source of income(yes/no): "<<endl;
+} 
+if(option ==6){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==7){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==8){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==9){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl;
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+cin>>option;
+
+if(option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==2){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==3){ 
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==4)
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==5){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==6){
+cout<<"Why You need a Scholarship: "<<endl;
+}
+if( option ==7){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==8){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==9){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl;
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+cin>>option;
+
+if(option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==2){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==3){ 
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==4)
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==5){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==6){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==7){
+cout<<"Enter the Sector Name: "<<endl;
+} 
+if(option ==8){
+cout<<"you have no record yet"<<endl;
+}
+if( option ==9){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl;
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+cin>>option;
+
+if(option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==2){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==3){ 
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==4)
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==5){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==6){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==7){
+cout<<"you cannot add more information"<<endl;
+}
+if (option ==8){
+cout<<"st1_name<<"\t"<<st1_fsc<<"\t"<<st1_semester_GPA<<endl;
+cout<<"st2_name<<"\t"<<st2_fsc<<"\t"<<st1_semester_GPA<<endl;
+cout<<"st3_name<<"\t"<<st3_fsc<<"\t"<<st1_semester_GPA<<endl;
+cout<<"st4_name<<"\t"<<st4_fsc<<"\t"<<st1_semester_GPA<<endl;
+cout<<"st5_name<<"\t"<<st5_fsc<<"\t"<<st1_semester_GPA<<endl;
+cout<<"st6_name<<"\t"<<st6_fsc<<"\t"<<st1_semester_GPA<<endl;
+}
+if( option ==9){
+cout<<"you have no record yet"<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl;
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+ cin>>option;
+
+if(option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==2){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==3){ 
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==4)
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==5){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==6){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==7){
+cout<<"you cannot add more information"<<endl;
+}
+if (option ==8){
+cout<<"you cannot add more information"<<endl;
+}
+
+if(option==9){
+float merit1 =st1_fsc / total_marks*40 +st1_semester_GPA /total_GPA*60;
+float merit2 =st2_fsc / total_marks*40 +st2_semester_GPA /total_GPA*60;
+float merit3 =st3_fsc / total_marks*40 +st3_semester_GPA /total_GPA*60;
+float merit4 =st4_fsc / total_marks*40 +st4_semester_GPA /total_GPA*60;
+float merit5 =st5_fsc / total_marks*40 +st5_semester_GPA /total_GPA*60;
+float merit6 =st6_fsc / total_marks*40 +st6_semester_GPA /total_GPA*60;
+
+  if(merit1>merit2){
+    cout<<"your application for scholarship is submit "<<endl;
+   if(merit1>merit3){
+    cout<<"your application for scholarship is submit "<<endl;
+
+ if(merit1>merit4){
+   cout<<"your application for scholarship is submit "<<endl;
+
+ if(merit1>merit5){
+   cout<<"your application for scholarship is submit "<<endl;
+
+ if(merit1>merit6){
+   cout<<"your application for scholarship is submit "<<endl;
+}
+if(option ==10){
+cout<<"you have no record yet"<<endl;
+
+cout<<"******************************************************************"<<endl;
+cout<<"*          scholarships Management system                        *"<<endl;
+cout<<"******************************************************************"<<endl;
+
+cout<<"Main Menu> "<<endl;
+cout<<"select the one of the option from the followings "<<endl;
+cout<<"1- Add the Student Information "<<endl;
+cout<<"2- Personal Information  "<<endl;
+cout<<"3- Field of study "<<endl;
+cout<<"4- Hobbies/Activities  "<<endl;
+cout<<"5- Family financial information"<<endl;
+cout<<"6- Personal statements "<<endl;
+cout<<"7- sector name "<<endl; 
+cout<<"8- View All Student "<<endl; 
+cout<<"9- Print the Records in Merit order"<<endl;
+cout<<"10- Verfication "<<endl;
+cout<<"11- Exit "<<endl;
+cout<<"Your option ";
+ cin>>option;
+
+if(option ==1){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==2){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==3){ 
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==4)
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==5){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==6){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==7){
+cout<<"you cannot add more information"<<endl;
+}
+if (option ==8){
+cout<<"you cannot add more information"<<endl;
+}
+if(option==9){
+cout<<"you cannot add more information"<<endl;
+}
+if(option ==10){
+  merit>=90
+cout<<"you are eligible for a scholarshipe"<<endl;
+
+if (option==11){
+cout<<exit<<endl;
+}
+
+
+
+
+
